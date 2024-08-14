38be206f40a85734db6f7945c7d859b2701fcc0d
Ferdivich <yvesedi41@gmail.com>
2024-08-14 13:09:05 +0000

    Database 1

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -13,8 +13,13 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
+            $table->string('name')->nullable();
+            $table->string('username');
+            $table->string('email')->unique()->nullable();
+            $table->string('phone_number')->unique();
+            $table->string('blood_type')->nullable();
+            $table->json('hospital')->nullable();
+            $table->string('photo')->nullable();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
---
+++ database/migrations/2024_08_13_183146_create_emergencies_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('emergencies', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('address');
+            $table->float('longitude');
+            $table->float('latitude');
+            $table->string('phone_number_1');
+            $table->string('phone_number_2')->nullable();
+            $table->string('phone_number_3')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('emergencies');
+    }
+};
